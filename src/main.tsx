1642d135a9209e15c7a0288f1f171e35c3470185
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2024-10-22 22:23:59 -0600

    Refactor layout and add search functionality

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
       { path: '/certificaciones', element: <Certificaciones /> },
     ],
   }, 
+  
 ])
 
 createRoot(document.getElementById('root')!).render(
